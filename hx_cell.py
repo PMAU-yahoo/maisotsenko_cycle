6d5d4d4cfac9fe43f5685d26615587a85035d01c
p_monsalvete <pilar.monsalvete@concordia.ca>
2025-04-01 13:58:53 +0200

    New (and better) structured code

--- hx_cell.py
+++ hx_cell.py
@@ -4,29 +4,43 @@ class HXCell:
   """
   This class models the heat exchanged in one single cell known the input properties of both flows
   """
-  def __init__(self,
-               working_temperature_in,
-               working_relative_humidity_in,
-               product_temperature_in,
-               product_relative_humidity_in):
+  def __init__(self, length):
     # todo: add any other required inputs, like dimensions
-    self._working_temperature_in = working_temperature_in
-    self._working_relative_humidity_in = working_relative_humidity_in
-    self._product_temperature_in = product_temperature_in
-    self._product_relative_humidity_in = product_relative_humidity_in
+    self._length = length
+    self._working_temperature_in = None
+    self._working_relative_humidity_in = None
+    self._product_temperature_in = None
+    self._product_relative_humidity_in = None
     self._working_temperature_out = None
     self._working_relative_humidity_out = None
     self._product_temperature_out = None
     self._product_relative_humidity_out = None
 
-    self._heat_exchange_model()
-
-  def _heat_exchange_model(self):
+  def heat_exchange_model(self):
     # todo: add all equations using the variables defined in __init__ and erase this:
     self._working_temperature_out = self._working_temperature_in
-    self._working_relative_humidity_out = self._working_relative_humidity_in
     self._product_temperature_out = self._product_temperature_in
+    self._latent_exchange()
+
+  def _latent_exchange(self):
+    self._working_relative_humidity_out = self._working_relative_humidity_in
     self._product_relative_humidity_out = self._product_relative_humidity_in
+
+  @property
+  def working_temperature_in(self):
+    """
+    Gets the input temperature of the working flow in ºC
+    :return: float
+    """
+    return self._working_temperature_in
+
+  @working_temperature_in.setter
+  def working_temperature_in(self, value):
+    """
+    Sets the input temperature of the working flow in ºC
+    :param value: float
+    """
+    self._working_temperature_in = value
 
   @property
   def working_temperature_out(self):
--- main.py
+++ main.py
@@ -7,10 +7,12 @@ working_temperature_in = 25   # ºC
 working_relative_humidity_in = 40   # %
 product_temperature_in = 35   # ºC
 product_relative_humidity_in = 40   # %
+length = 0.1  # m
 
-cell = HXCell(working_temperature_in,
-              working_relative_humidity_in,
-              product_temperature_in,
-              product_relative_humidity_in)
+cell = HXCell(length)
+
+cell.working_temperature_in = working_temperature_in
+
+cell.heat_exchange_model()
 
 print(cell.working_temperature_out)
